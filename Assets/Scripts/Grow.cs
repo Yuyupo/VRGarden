9869a10d9899eded700ec52f8cff69657d721d58
Daniella Bársony <bella@inf.u-szeged.hu>
2018-10-07 10:07:36 +0200

    Hmmmm Assets/SteamVR/InteractionSystem/Core/Scripts/LinearAudioPitch.cs.meta

---
+++ Assets/Scripts/Grow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grow : MonoBehaviour {
+	public GameObject small;
+	public GameObject big;
+
+	void Start () {
+		InvokeRepeating("grow", 5.0f, 0.0f);
+	}
+
+	public void grow(){
+		small.SetActive (false);
+		big.SetActive(true);
+	}
+}
---
+++ Assets/Scripts/Trunk.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trunk : MonoBehaviour {
+	public GameObject tree;
+	public GameObject grownTree;
+	public int lifePoint;
+	public List<Branch> branches;
+	private bool dead;
+
+	void Start()
+	{
+		// How often should your tree try to suicide :c
+		InvokeRepeating("rotting", 1.0f, 100.0f);
+	}
+
+	public void rotting(){
+		Branch branch = GetComponent<Branch>();
+		// If there is a branch rot it
+		if (branch) {
+			branch.childRotter ();
+		}
+	}
+}
